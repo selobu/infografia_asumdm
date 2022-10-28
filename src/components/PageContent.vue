4db98f53d497bf2e211d48d36677378e6fad64ce
Sebastian Lopez Buritica <selobu@gmail.com>
2022-10-28 18:29:27 -0500

    adding gravatar

--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -2,16 +2,16 @@
   <v-container>
     <v-row class="text-center" max-width="900px">
       <v-col cols="12">
-        <v-img
-          src="https://gestionhseq.com/assets/img/Logotipo.svg"
-          class="my-2"
-          contain
-          height="50"
-        />
+        <v-avatar>
+          <v-img
+            src="https://www.gravatar.com/avatar/302c652af1a74abdadf3dcaebd6513ee?s=250"
+            alt="Sebastian López"
+          ></v-img>
+        </v-avatar>
       </v-col>
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          BIENVENIDO A LA INFOGRAFÍA
+          INFOGRAFÍA ASUM-DM
         </h1>
       </v-col>
     </v-row>
@@ -46,14 +46,14 @@
         </Card>
       </v-col>
       <v-col cols="6">
-            <v-btn
-              icon="mdi-refresh"
-              size="large"
-              color="primary"
-              @click="onrefresh"
-              title="Reiniciar"
-              label="Reinciar"
-            ></v-btn>
+        <v-btn
+          icon="mdi-refresh"
+          size="large"
+          color="primary"
+          @click="onrefresh"
+          title="Reiniciar"
+          label="Reinciar"
+        ></v-btn>
       </v-col>
     </v-row>
   </v-container>
@@ -71,8 +71,10 @@ export default {
       card1: {
         title: "Fase 1",
         subtitle: "Inicio del proyecto",
-        image: "https://2.bp.blogspot.com/-FRa4mAipzcA/Vf8gYlkPa8I/AAAAAAAAAg0/IMAUboWhtJo/s1600/INICIO.png",
-        content: "Lo primero es realizar un levantamiento de la información, \
+        image:
+          "https://2.bp.blogspot.com/-FRa4mAipzcA/Vf8gYlkPa8I/AAAAAAAAAg0/IMAUboWhtJo/s1600/INICIO.png",
+        content:
+          "Lo primero es realizar un levantamiento de la información, \
           se debe hacer un diagnóstico respecto a qué tan preparada está la\
           compañía para iniciarse en un proyecto de analítica, dentro de esta\
           etapa se deben identificar factores como:<br/><br/>\
@@ -90,8 +92,10 @@ export default {
       card2: {
         title: "Fase 2",
         subtitle: "Enternder el negocio",
-        image: "https://agenciadomus.com.mx/blog/wp-content/uploads/2019/06/blog-e1560797059757-1140x641.jpg",
-        content: "En esta etapa es importante entender cuáles son esos objetivos,\
+        image:
+          "https://agenciadomus.com.mx/blog/wp-content/uploads/2019/06/blog-e1560797059757-1140x641.jpg",
+        content:
+          "En esta etapa es importante entender cuáles son esos objetivos,\
          pero, además, es fundamental reconocer los requerimientos y las necesidades\
          organizacionales, al identificar este aspecto se podrán reconocer aquellos\
          problemas que podrían resolverse con la implementación de la analítica",
@@ -102,7 +106,8 @@ export default {
         title: "Fase 3",
         subtitle: "Descubrimiento y entendimiento de los datos",
         image: "https://miro.medium.com/max/3000/1*0o44SgYfO_NgbEbqRICftw.png",
-        content: "Recolección inicial de datos para determinar la consistencia de la información:\
+        content:
+          "Recolección inicial de datos para determinar la consistencia de la información:\
          Fuente de datos, compra de datos, datos abiertos",
         showcurrent: false,
         desablenextbtn: false,
@@ -110,8 +115,10 @@ export default {
       card4: {
         title: "Fase 4",
         subtitle: "Preparación de los datos",
-        image: "https://itcomunicacion.com.mx/wp-content/uploads/2020/02/datDeca-768x427.jpg",
-        content: "Seleccionar los datos, filtrarlos, completar los datos que haganfalta,\
+        image:
+          "https://itcomunicacion.com.mx/wp-content/uploads/2020/02/datDeca-768x427.jpg",
+        content:
+          "Seleccionar los datos, filtrarlos, completar los datos que haganfalta,\
          integración de los datos, homogenizar.",
         showcurrent: false,
         desablenextbtn: false,
@@ -119,8 +126,10 @@ export default {
       card5: {
         title: "Fase 5",
         subtitle: "Construcción del modelo",
-        image: "https://c5e6g5f8.rocketcdn.me/wp-content/uploads/2014/10/pexels-photo-534220.jpeg",
-        content: "Escoger la técnica para realizar el modelo, diseñar pruebas, controuir el modelo",
+        image:
+          "https://c5e6g5f8.rocketcdn.me/wp-content/uploads/2014/10/pexels-photo-534220.jpeg",
+        content:
+          "Escoger la técnica para realizar el modelo, diseñar pruebas, controuir el modelo",
         showcurrent: false,
         desablenextbtn: false,
       },
@@ -128,12 +137,13 @@ export default {
         title: "Fase 6",
         subtitle: "Evaluar el modelo",
         image: "https://miro.medium.com/max/1135/1*cwEeGqeSP5h5MXFm67lD3w.png",
-        content: "Verificar si el resultado cumple con las metas del modelo,\
+        content:
+          "Verificar si el resultado cumple con las metas del modelo,\
          sirve para realizar un mejor entendimiento de los datos de la empresa?\
          los datos generan una nueva analítica\
          verificar si responde a las expectativas.",
         showcurrent: false,
-        desablenextbtn: false,
+        desablenextbtn: true,
       },
     },
     show: false,
@@ -153,7 +163,7 @@ export default {
           showcurrent: u === 1 ? true : false,
           desablenextbtn: u === lencontents ? true : false,
         };
-        u+=1;
+        u += 1;
       }
       this.contents = { ...newitems };
     },
